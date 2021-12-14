9018045ea3e61616500aa46ad866d2a43a7d4608
Devin <devinallen26@gmail.com>
2021-12-14 23:31:50 +0000

    TestTodoList class is made and has testEmptyList ready but not implemented yet

---
+++ test/TestTodoList.php
@@ -0,0 +1,10 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class TestTodoList extends TestCase 
+{
+    public function testEmptyList() {
+        //  $todoList = new TodoList();
+        self::assertTrue(true);
+    }
+}
--- test/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-use PHPUnit\Framework\TestCase;
-
-class test extends TestCase 
-{
-    public function testSimpleReturnTrue() {
-        $this->assertTrue(true);
-    }
-}
