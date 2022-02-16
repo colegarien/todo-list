3151e488fe12bc26b529c3880013350e90c5a465
Devin <devinallen26@gmail.com>
2022-02-16 12:34:27 -0600

    Finished Test Empty List and started on Test Add To List

--- src/TodoList.php
+++ src/TodoList.php
@@ -3,5 +3,15 @@ namespace Todo;
 
 class TodoList 
 {
+    private array $list = [];
 
+    public function add(string $item): void
+    {
+        $this->list[] = $item; 
+    }
+
+    public function isEmpty(): bool
+    {
+        return empty($this->list);
+    }
 }
--- test/TodoListTest.php
+++ test/TodoListTest.php
@@ -9,7 +9,13 @@ class TodoListTest extends TestCase
 {
     public function testEmptyList() {
         $todoList = new TodoList();
-        $expected = TodoList::class;
-        self::assertInstanceOf($expected, $todoList);
+        self::assertTrue($todoList->isEmpty());
+    }
+
+    public function testAddToList() {
+        $todoList = new TodoList();
+        $todoList->add("item 1");
+        self::assertFalse($todoList->isEmpty());
+        // todo: need to assert that we get the expected item when we try to retrieve the added item with this test or another one
     }
 }
