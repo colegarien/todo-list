90b002b5f219c8e43ae4ef1f3d13d70d123675a3
Cole Garien <colegarien@gmail.com>
2022-04-06 16:40:47 +0000

    fix include

--- test/TodoListTest.php
+++ test/TodoListTest.php
@@ -1,7 +1,7 @@
 <?php
 namespace Todo\Test;
 use PHPUnit\Framework\TestCase;
-include_once __DIR__ . "../vendor/autoload.php";
+include_once __DIR__ . "/../vendor/autoload.php";
 
 use Todo\TodoList;
 
