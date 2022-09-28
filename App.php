e906d665ba89f892aae006708f1828510b4a0cbc
Devin <devinallen26@gmail.com>
2022-09-28 17:34:40 +0000

    Added more to UI defined functionlality of how we want it to work

--- App.php
+++ App.php
@@ -1,15 +1,19 @@
 <?php
 /**
- * UI Functionality.. Use Case???
+ * ***UI Functionality.. Use Case??? Maybe after UI functionality is defined
  * 1. Welcome To The Todo App
-* 2. Show Empty Todo List
-* 	- Tell user at any time they can exit by entering "exit"
-* 3. Allow user to Add Items Into The List
+* 2.Tell user at any time they can exit an action by entering "~exit". Continue (yes/~exit)?
+* 3. Allow user to Manage Items Into The List
 * 	- What Action would you like to take?
-		*Add Item
-		*Remove Item
-		*Show Item
-		*Complete Item
+		*1 - Add Item
+		*2 - Remove Item
+		*3 - Show Item
+		*4 - Complete Item
+        *5 - Uncomplete Item
+    * Add Item UI
+    *   Add Item: _____
+    *   User can enter ~exit to go back to main menu
+    *
  */
 $keepEditingTodoList = true;
 $handle = fopen ("php://stdin","r");
